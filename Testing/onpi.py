0c5253370208a2b2115bf54afb83e92acda99444
Nicholas George <56926399+nicholaskgeorge@users.noreply.github.com>
2020-08-30 20:57:50 -0400

    Gave drone message test unique name in code

--- Testing/onpi.py
+++ Testing/onpi.py
@@ -2,7 +2,7 @@ import sys
 import time
 sys.path.append(r'/home/pi/Desktop/Autonomous-Following-Drone/')
 from messaging import Messages
-remote = Messages()
+remote = Messages('Drone')
 remote.begin()
 remote.startsub("test/message")
 while True:
