ceaef2bc21dadb6467dd54ec3d68722eb1459b95
Nicholas George <56926399+nicholaskgeorge@users.noreply.github.com>
2020-11-16 08:28:10 -0500

    begining work on ad-hoc switcher

---
+++ ad-hoc switcher.py
@@ -0,0 +1,2 @@
+f = open(r"C:\Users\Nico\Autonomous-Following-Drone\testingread.txt","r")
+print(f.read())
--- testremote.py
+++ testremote.py
@@ -10,8 +10,30 @@ while True:
             message.send('up')
         elif keyboard.is_pressed('s'):
             message.send('down')
+        """
+        elif keyboard.is_pressed('w'):
+            message.send('forward')
+        elif keyboard.is_pressed('s'):
+            message.send('back')
+        elif keyboard.is_pressed('a'):
+            message.send('left')
+        elif keyboard.is_pressed('d'):
+            message.send('right')
+        elif keyboard.is_pressed('q'):
+            message.send('yawleft')
+        elif keyboard.is_pressed('e'):
+            message.send('yawright')
+        elif keyboard.is_pressed('`'):
+            message.send('land')
+        """
+        """
+        This is constantly sent when no buttons are pressed. It is a way for The
+        drone to know that we still have communication with the computer. If the
+        drone does not recive a message of some kind within a certian time period
+        it will automatially descend and land.
+        """
         else:
-            message.send('stable')
+            message.send('p')
             delay(0.5)
     except:
         pass
